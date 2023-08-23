e539977d11792b815cefe0cc5d41f6d32973b93a
sureshkumar-devops <suresh.ak47gmail.com>
2023-08-23 05:53:38 +0530

    Added initial Commit

---
+++ src/main/java/com/w3logics/seo_project/App.java
@@ -0,0 +1,13 @@
+package com.w3logics.seo_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! w3logics" );
+    }
+}
